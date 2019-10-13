843074a8065509fb47d2df81b3e286b90bd057fe
Mal <malrsteffes@gmail.com>
2019-10-13 11:02:05 -0400

    added JawsDB connection configuration

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root1234",
-  database: "burgers_db"
-});
+var connection;
+
+// if the server contains the JAWSDB_URL environmental variable (saved by Heroku), it connects
+// to the JawsDB database
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+// if the server lacks the variable, it falls back on the explicitly defined local db
+else {
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root1234",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
