4bea201d70c6ddf5d8d60793e2522c8b34d6d0da
Mal <malrsteffes@gmail.com>
2019-10-13 12:21:10 -0400

    fixed incorrect port in server.js

--- config/connection.js
+++ config/connection.js
@@ -7,20 +7,21 @@ require("dotenv").config();
 var connection;
 
 // if the server contains the JAWSDB_URL environmental variable (saved by Heroku), it connects
-// to the JawsDB database
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} 
+// // to the JawsDB database
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } 
 // if the server lacks the variable, it falls back on the explicitly defined local db
-else {
-  mysql.createConnection({
+// else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root1234",
     database: "burgers_db"
   });
-};
+// };
+console.log(connection);
 
 // Make connection.
 connection.connect(function(err) {
