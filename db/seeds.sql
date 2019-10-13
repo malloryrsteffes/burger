1235a9614885555d74bbb7091b7f33d9ef7be6b1
Mal <malrsteffes@gmail.com>
2019-10-13 10:07:34 -0400

    added schema, seed, and connection files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Butter Burger');
+INSERT INTO burgers (name) VALUES ('The Krusty Burger');
+INSERT INTO burgers (name) VALUES ('The Krabby Patty');
+
+
+
